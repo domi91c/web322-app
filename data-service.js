c71d179c925e0f7ff9a7b99a43df7c36c4bd9e96
domi91c <dominic.n@me.com>
2017-10-08 22:32:45 -0400

    assignment 3 completed

---
+++ data-service.js
@@ -0,0 +1,125 @@
+var fs = require('fs')
+
+var employees = []
+var departments = []
+
+module.exports.initialize = () =>
+    new Promise((resolve, reject) => {
+      try {
+        fs.readFile('./data/employees.json', (err, data) => {
+          if (err) throw err
+          employees = JSON.parse(data)
+        })
+        fs.readFile('./data/departments.json', (err, data) => {
+          if (err) throw err
+          departments = JSON.parse(data)
+        })
+      } catch (ex) {
+        reject('Initialization Failed')
+      }
+      resolve('Working...')
+    })
+
+module.exports.getAllEmployees = () => {
+  var response = []
+  return new Promise((resolve, reject) => {
+    for (var i = 0; i < employees.length; i++) {
+      response.push(employees[i])
+    }
+    if (response.length === 0) {
+      reject('no results returned')
+    }
+    resolve(response)
+  })
+}
+
+module.exports.getEmployeesByStatus = status => {
+  var response = []
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i].status === status) {
+        response.push(employees[i])
+      }
+    }
+    if (response.length === 0) {
+      reject('no results returned')
+    }
+    resolve(response)
+  })
+}
+
+module.exports.getEmployeesByDepartment = department => {
+  var response = []
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i].department === department) {
+        response.push(employees[i])
+      }
+    }
+    if (response.length === 0) {
+      reject('no results returned')
+    }
+    resolve(response)
+  })
+}
+
+module.exports.getEmployeesByManager = manager => {
+  var response = []
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i].employeeManagerNum === manager) {
+        response.push(employees[i])
+      }
+    }
+    if (response.length === 0) {
+      reject('no results returned')
+    }
+    resolve(response)
+  })
+}
+
+module.exports.getEmployeeByNum = num =>
+    new Promise((resolve, reject) => {
+      for (let i = 0; i < employees.length; i++) {
+        if (employees[i].employeeNum === num) {
+          resolve(employees[i])
+        }
+      }
+      reject('no results returned')
+    })
+
+module.exports.getManagers = () => {
+  var response = []
+  return new Promise((resolve, reject) => {
+    if (employees.length === 0) {
+      reject('no results returned')
+    } else {
+      for (var i = 0; i < employees.length; i++) {
+        if (employees[i].isManager === true) {
+          response.push(employees[i])
+        }
+      }
+      if (response.length === 0) {
+        reject('no results returned')
+      }
+    }
+    resolve(response)
+  })
+}
+
+module.exports.getDepartments = () => {
+  var response = []
+  return new Promise((resolve, reject) => {
+    if (employees.length === 0) {
+      reject('no results returned')
+    } else {
+      for (var i = 0; i < departments.length; i++) {
+        response.push(departments[i])
+      }
+      if (response.length === 0) {
+        reject('no results returned')
+      }
+    }
+    resolve(response)
+  })
+}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /**********************************************************************************
- *  WEB322 –Assignment2
- *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  WEB322 –Assignment3
+ *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
  *  No part of this assignment has been copied manually or electronically from any other source
  *  (including web sites) or distributed to other students.
  *
@@ -9,15 +9,28 @@
  *  Online (Heroku) URL: http://web322-app1.herokuapp.com/
  *********************************************************************************/
 
-
+let express = require('express')
+let app = express()
+let path = require('path')
+let data_service = require('./data-service.js')
 const HTTP_PORT = process.env.PORT || 8080
-const data = require('./data-service.js')
-const express = require('express')
-const app = express()
-const path = require('path')
+
+function onHttpStart() {
+  console.log('Express listening on port ' + HTTP_PORT)
+  return new Promise((res, req) => {
+    data_service.initialize()
+                .then(resp => {
+                  console.log(resp)
+                })
+                .catch(err => {
+                  console.log(err)
+                })
+  })
+}
+
+// load assets directory
 app.use(express.static('public'))
 
-// setup a 'route' to listen on the default url path
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/home.html'))
 })
@@ -26,5 +39,74 @@ app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/about.html'))
 })
 
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT)
+app.get('/employees', (req, res) => {
+  if (req.query.status) {
+    data_service.getEmployeesByStatus(req.query.status)
+                .then(resp => {
+                  res.json(resp)
+                })
+                .catch(err => {
+                  res.json({ message: err })
+                })
+  } else if (req.query.department) {
+    data_service.getEmployeesByDepartment(req.query.department)
+                .then(resp => {
+                  res.json(resp)
+                })
+                .catch(err => {
+                  res.json({ message: err })
+                })
+  } else if (req.query.manager) {
+    data_service.getEmployeesByManager(req.query.manager)
+                .then(resp => {
+                  res.json(resp)
+                })
+                .catch(err => {
+                  res.json({ message: err })
+                })
+  } else {
+    data_service.getAllEmployees()
+                .then(resp => {
+                  res.json(resp)
+                })
+                .catch(err => {
+                  res.json({ message: err })
+                })
+  }
+})
+
+app.get('/employee/:num', (req, res) => {
+  data_service.getEmployeeByNum(req.params.num)
+              .then(resp => {
+                res.json(resp)
+              })
+              .catch(err => {
+                res.json({ message: err })
+              })
+})
+
+app.get('/managers', (req, res) => {
+  data_service.getManagers()
+              .then(resp => {
+                res.json(resp)
+              })
+              .catch(err => {
+                res.json({ message: err })
+              })
+})
+
+app.get('/departments', (req, res) => {
+  data_service.getDepartments()
+              .then(resp => {
+                res.json(resp)
+              })
+              .catch(err => {
+                res.json({ message: err })
+              })
+})
+
+app.use((req, res) => {
+  res.status(404).send('404: page not found')
+})
+
+app.listen(HTTP_PORT, onHttpStart)
